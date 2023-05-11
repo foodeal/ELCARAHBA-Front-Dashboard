a199913933845c7f530290fb0510ac6f96ef166e
fibinacci <63007061+abdr1m@users.noreply.github.com>
2023-05-11 01:54:40 +0100

    Garage details

--- src/core/services/garagesServices.service.ts
+++ src/core/services/garagesServices.service.ts
@@ -5,20 +5,38 @@ import { GarageDTO } from "../generated/GarageDTO";
 import { GarageData } from "../models/garage/garage";
 
 async function getAllGarages(): Promise<GarageDTO[]> {
-  const headers = new Headers();
-  headers.append('Accept', 'application/json');
-  try {
-      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJpYXQiOjE2ODM1NTA0MTYsImV4cCI6MTY4NDE1NTIxNn0.nqmEB8lwxzIq19NVWIta3JXgS-q1RB7zBSdU6dUPhQw";
-      const res = await axios.get(`${apiUrl}/${ApiUrlsEnum.GetAllGarages}/`, {
-          headers: {
-              Authorization: `Bearer ${token}`,
-          },
-      });
-      return res.data.map((userData: any) => GarageData.mapToApiValue(userData));
-  } catch (error) {
-      console.error('Error fetching users:', error);
-      return [];
-  }
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    try {
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJpYXQiOjE2ODM1NTA0MTYsImV4cCI6MTY4NDE1NTIxNn0.nqmEB8lwxzIq19NVWIta3JXgS-q1RB7zBSdU6dUPhQw";
+        const res = await axios.get(`${apiUrl}/${ApiUrlsEnum.GetAllGarages}/`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return res.data.map((userData: any) => GarageData.mapToApiValue(userData));
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        return [];
+    }
 }
 
-export default { getAllGarages }
+async function getGarageDetails(id: number): Promise<GarageDTO | null> {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    try {
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExLCJpYXQiOjE2ODM1NTA0MTYsImV4cCI6MTY4NDE1NTIxNn0.nqmEB8lwxzIq19NVWIta3JXgS-q1RB7zBSdU6dUPhQw";
+        const res = await axios.get(`${apiUrl}/${ApiUrlsEnum.GetGarage}/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return GarageData.mapToApiValue(res.data);
+    } catch (error) {
+        console.error('Error fetching garage:', error);
+        return null;
+    }
+}
+
+
+export default { getAllGarages, getGarageDetails }
